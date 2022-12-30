747af9a45ff458e9d990f24803aa0892e58d88e3
unknown <tanishka98765@gmail.com>
2022-12-30 16:27:22 +0530

    removed useEffect

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import questions from '../questions';
 export default function QuestionBox() {
 
--- src/headings.js
+++ src/headings.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 function Heads(){
     const[currMode,changeMode]=useState('light mode');
     const modeSetting=()=>{
